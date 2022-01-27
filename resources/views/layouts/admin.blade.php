b4ed1c05f97bd9a52b3d600ae967c396818445ba
Arif-Iskandar <arif15iskandar@gmail.com>
2022-01-27 13:08:25 +0800

    update
    +logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -83,7 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="/home" class="brand-link">
-        <img src="{{ asset('images/logoparkir1.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
+        <img src="{{ asset('images/logoparkir2.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">eParkir</span>
       </a>
 
